b97c0ede67bda4e7c91dc79bbafabc9e64a4b2a3
Sergey Mironov <sergeymironov@protonmail.com>
2025-10-24 21:55:10 +0300

    Add api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -138,8 +138,8 @@ class Api {
       .then(getResponse)
   }
   }
-  // Замените на адрес вашего бэкенда
-  const api = new Api('http://localhost:3000');
+
+  const api = new Api('https://nodejs-mesto-project.onrender.com');
   
   export default api;
   
